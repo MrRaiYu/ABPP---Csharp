5b5f35e92d00a6d235cc2cf5238673fd671c5d80
bilal <bboukhorissa@cciproductions.fr>
2023-03-03 08:09:06 +0100

    Projet ABBP C# : login

---
+++ Projet ABPP/Batiment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_ABPP
+{
+    public class Batiment
+    {
+        public string Nom { get; set; }
+        public string Emplacement { get; set; }
+        public string Description { get; set; }
+
+        // Constructeur par défaut
+        public Batiment()
+        {
+            Nom = "";
+            Emplacement = "";
+            Description = "";
+        }
+
+        // Constructeur avec paramètres
+        public Batiment(string nom, string emplacement, string description)
+        {
+            Nom = nom;
+            Emplacement = emplacement;
+            Description = description;
+        }
+    }
+}
